c314834bc0fe3055416323169a2f02f355156f95
lim <dla0335@naver.com>
2015-12-23 11:16:22 +0900

    My Django Girls app, 두번째 커밋

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
+python
 import os
+import sys
+
+path = '/home/jihoono335/my-first-blog' # 여러분의 유저네임을 여기에 적어주세요.
+if path not in sys.path:
+	sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
