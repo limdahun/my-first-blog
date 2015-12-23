2c850d9fe9ee5cca6fe699c430c760af36a73305
lim <dla0335@naver.com>
2015-12-23 11:47:47 +0900

    Changed the HTML for the site.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-path = '/home/jihoono335/my-first-blog' # 여러분의 유저네임을 여기에 적어주세요.
+path = '/home/jihoono335/my-first-blog' 
 if path not in sys.path:
 	sys.path.append(path)
 
@@ -9,4 +9,5 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
 application = DjangoWhiteNoise(get_wsgi_application())
